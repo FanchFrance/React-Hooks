7399b61d9fc2ef157d8eb9abf608ad91919d7b84
wilder <francois.yhuel@hotmail.fr>
2020-04-26 10:40:59 +0200

    init counter

---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  // Déclare une fonction setCount qui permet de modifier la valeur de count
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Le compteur est  à : {count} </p>
+      <input type="number" value={count} onChange={e => setCount(e.target.count)} ></input>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+      
+    </div>
+  );
+}
+
+export default Counter;
